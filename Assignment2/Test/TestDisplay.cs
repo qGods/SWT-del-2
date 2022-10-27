78d59cd064f1f869cced74bb4087aeab12a0d6e9
Akif-Balikci <akifbalikci00@gmail.com>
2022-10-27 17:10:51 +0200

    test til display

--- Assignment2/Test/TestDisplay.cs
+++ Assignment2/Test/TestDisplay.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Library.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.UtiAndSim;
+using NUnit.Framework;
 
 namespace Test
 {
-    internal class TestDisplay
+    public class TestDisplay
     {
+        private IDisplay _uut;
+        private StringWriter sw;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+            sw = new StringWriter();
+            Console.SetOut(sw);
+        }
+
+        [Test]
+        public void connectPhoneTest()
+        {
+            _uut.connectPhone();
+            Assert.That(sw.ToString(), Contains.Substring("Please connect your phone"));
+        }
     }
 }
