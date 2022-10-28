d850c3a1c247de03d58700e82754f99af59467ea
qGods <batu12600@outlook.com>
2022-10-28 18:50:46 +0200

    lll

--- Library/Interface/IStationControl.cs
+++ Library/Interface/IStationControl.cs
@@ -10,8 +10,6 @@ namespace Library.Interface
     public interface IStationControl
     {
         StationControl.LadeskabState _state { get; set; }
-        void OnDoorStateChange(object? door, DoorStateEventArgs doorArgs);
-        void OnRFIDDetected(object? rfidReader, rfIDDetectedArgs rfidArgs);
         int _oldID { get;}
 
     }
